c42264a7df0569582493dce79d60227afb9c4808
LisadiKaprio <lisadikaprio@gmail.com>
2024-01-21 19:08:22 +0100

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,26 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+export default defineNuxtConfig({
+  // ...
+  build: {
+    transpile: ['vuetify'],
+  },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+    // ...
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+  css: ['~/assets/main.scss'],
+  devtools: { enabled: true },
+})
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <v-app>
+    <v-sheet :elevation="4" :height="200" :width="200" rounded class="justify-self-center align-self-center">
+      <h1>Hi!</h1>
+      <p>
+        I’m a Game Design graduate. I enjoy creating 2D artworks and game assets, as well as developing web applications.
+      </p>
+    </v-sheet>
+  </v-app>
+</template>
